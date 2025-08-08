d2355d209fe27e9cf00fcc83e70796c2481c42df
Plymou <128940651+Plymou@users.noreply.github.com>
2025-08-09 00:11:21 +0100

    Add files via upload

---
+++ price test.py
@@ -0,0 +1,13 @@
+price=10
+rating= 4.9
+
+
+# name= rasakii
+
+# correction for the above is below
+name= 'rasakii'
+# This is because english words must be enclosed in quotes
+
+
+is_published = True
+print(price)
